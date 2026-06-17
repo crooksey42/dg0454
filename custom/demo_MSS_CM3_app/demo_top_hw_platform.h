9f97bbda505e59eb14864a1b295a328764bc68aa
crook <crooksey42@gmail.com>
2026-06-17 16:57:55 -0400

    added custom code

---
+++ custom/demo_MSS_CM3_app/demo_top_hw_platform.h
@@ -0,0 +1,16 @@
+#ifndef demo_top_HW_PLATFORM_H_
+#define demo_top_HW_PLATFORM_H_
+/*****************************************************************************
+*
+*Created by Microsemi SmartDesign  Tue Mar 21 11:53:03 2017
+*
+*Memory map specification for peripherals in demo_top
+*/
+
+/*-----------------------------------------------------------------------------
+* CM3 subsystem memory map
+* Master(s) for this subsystem: CM3 
+*---------------------------------------------------------------------------*/
+
+
+#endif /* demo_top_HW_PLATFORM_H_*/
