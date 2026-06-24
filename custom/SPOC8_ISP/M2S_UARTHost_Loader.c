22bfd344a15b5b10e86ee55701784177ea209fab
crook <crooksey42@gmail.com>
2026-06-24 10:15:45 -0400

    added mode 3, nvm direct copy of hex to nvm

--- custom/SPOC8_ISP/M2S_UARTHost_Loader.c
+++ custom/SPOC8_ISP/M2S_UARTHost_Loader.c
@@ -53,6 +53,14 @@ void SerialPutc(HANDLE hCom, char txchar)
 	return;
 }
 
+void print_char_n(const char buf[8], int len)
+{
+    for (int i = 0; i < len; i++)
+    {
+    	printf ("%c", buf[i]);
+    }
+    printf ("\n");
+}
 
 int main(int argc, char *argv[])
 {
@@ -63,7 +71,7 @@ int main(int argc, char *argv[])
 	long size,result,length,address,factor,temp;
 	char filename[100];
 	char buffer[8192];
-	char ret[8];
+	char ret[16];
 	char filesize[8];
 	BOOL bReadRC;
 	static DWORD iBytesRead = -1;
@@ -85,10 +93,10 @@ int main(int argc, char *argv[])
 	strcpy(Action_code,argv[3]);
 	//printf("mode = %s\n",Action_code);
     
-    if( Action_code[0] != '0' && Action_code[0] != '1' &&  Action_code[0] != '2')
+    if( Action_code[0] != '0' && Action_code[0] != '1' &&  Action_code[0] != '2' &&  Action_code[0] != '3')
     {
    	    printf("Usage for M2S_UARTHost_Loader: M2S_UARTHost_Loader.exe filename comportnumber mode\n");
-		printf("ISP modes : \nmode = 0 -> Authenticate ; mode = 1 -> program ; mode = 2 -> verify\n");
+		printf("ISP modes : \nmode = 0 -> Authenticate ; \nmode = 1 -> program ; \nmode = 2 -> verify; \nmode = 3 -> nvm direct copy\n");
 
 		return 0;
     }
@@ -124,7 +132,8 @@ int main(int argc, char *argv[])
 
 	// Fill in DCB: 57,600 bps, 8 data bits, no parity, and 1 stop bit.
 
-	dcb.BaudRate = 460800;//CBR_57600; // set the baud rate
+	dcb.BaudRate =
+			460800;//CBR_57600; // set the baud rate
 	dcb.ByteSize = 8; // data size, xmit, and rcv
 	dcb.Parity = NOPARITY; // no parity bit
 	dcb.StopBits = ONESTOPBIT; // one stop bit
@@ -245,8 +254,8 @@ int main(int argc, char *argv[])
 		printf ("recd 'z'\n");
         _itoa(size,filesize,10);       
 		bWriteRC = WriteFile(hCom, filesize, 8, &iBytesWritten,NULL);
-
 	}
+
 	//Read the address and return bytes from target and transmit the requested number of bytes from that address to the target.
 	//At the target side the data will be stored in internal memory and IAP will programm the FPGA with the stored data from internal memory.
 
@@ -254,6 +263,7 @@ int main(int argc, char *argv[])
 	{  
 		//read the message from the target.
 		bReadRC = ReadFile(hCom, ret, 1,&iBytesRead,NULL); 
+		printf ("recd %c(0x%02X)\n", ret[0], ret[0]);
 
         if( Action_code[0] == '2')
         {
@@ -275,7 +285,7 @@ int main(int argc, char *argv[])
         {
             if(ret[0] == 'p')
             {
-                 printf("\nISP Authentication completed successfully\n");
+                 printf("\nISP Authentication completed successfully 2\n");
                  return (0);
             }
             else if(ret[0] == 'q')
@@ -287,6 +297,22 @@ int main(int argc, char *argv[])
                 return (0);
             }
         }
+        else if( Action_code[0] == '3')
+		{
+			if(ret[0] == 'p')
+			{
+				 printf("\nNVM copy direct completed successfully 2\n");
+				 return (0);
+			}
+			else if(ret[0] == 'q')
+			{
+				printf("\nNVM copy direct failed");
+				temp = 0;
+				bReadRC =ReadFile(hCom, (char *)&temp, 8,&iBytesRead,NULL);
+				printf(" with error code = %d\n",temp);
+				return (0);
+			}
+		}
         
         if(ret[0]!='b')
 			continue;
@@ -360,11 +386,10 @@ int main(int argc, char *argv[])
 		while((returnbytes-1)/factor)
 		{
 		  crc = crc^buffer[factor];
+		  printf ("scrc: 0x%02X ", crc);
 		  factor = factor*2;
 		}
 		bWriteRC = WriteFile(hCom, &crc, 1, &iBytesWritten,NULL);
-		bReadRC = ReadFile(hCom, ret, 1,&iBytesRead,NULL);
-
 
 		char hexstr[24];
 		snprintf(hexstr, sizeof(hexstr),
@@ -378,7 +403,7 @@ int main(int argc, char *argv[])
 		             (unsigned char)ret[1],
 		             (unsigned char)ret[0]);
 
-		printf ("sent crc: %02X, recd: %s\n", crc, hexstr);
+		printf ("\nsent crc: 0x%02X, recd: %s\n", crc, hexstr);
 
 		if(bReadRC==0) {
 		  printf("Read Ack operation fail from target for the requested bytes\n");
@@ -388,6 +413,7 @@ int main(int argc, char *argv[])
 		}
         //CRC check ends
 		fclose(fp);
+
 		if(address+returnbytes == size)
         {
 			if(Action_code[0] == '1')
@@ -399,17 +425,21 @@ int main(int argc, char *argv[])
 			if(ret[0]=='p')
 			{
 				if(Action_code[0] == '0')
-				printf("\n*********ISP Authentication completed successfully*******\n");
+					printf("\n*********ISP Authentication completed successfully*******\n");
 				else if(Action_code[0] == '2')
-				printf("\n*********ISP Verification completed successfully*******\n");
+					printf("\n*********ISP Verification completed successfully*******\n");
+				else if(Action_code[0] == '3')
+					printf("\n*********ISP Verification completed successfully*******\n");
 				return (0);
 			}
 			else if(ret[0]=='q')
 			{
 				if(Action_code[0] == '0')
-				printf("\n*********ISP Authentication failed\n");
+					printf("\n*********ISP Authentication failed\n");
 				else if(Action_code[0] == '2')
-				printf("\n*********ISP Verification failed\n");
+					printf("\n*********ISP Verification failed\n");
+				else if(Action_code[0] == '3')
+					printf("\n*********ISP Verification failed\n");
 				temp = 0;
 				bReadRC =ReadFile(hCom, (char *)&temp, 8,&iBytesRead,NULL);
 				printf(" with error code = %d\n",temp);
